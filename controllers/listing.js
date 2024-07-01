97443fca085e0bd856de0f017fb7a2c5f5989f8e
Vachas Pati Pandey <121096492+VHAC-code@users.noreply.github.com>
2024-07-01 10:28:36 +0530

    Update listing.js
    comments removed

--- controllers/listing.js
+++ controllers/listing.js
@@ -1,9 +1,9 @@
-//3(a) we are created this folder and this file in  3(a) lec where we will make our file more readable by copying all the routes async function from respected.js grom routes and will copy here and will exports from here
+
 const Listing = require("../models/listing");
 
-const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');//3(b) geocoding lec and this is required from git mapbox-sdk-js
+const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
 const mapToken = process.env.MAP_TOKEN;//token required from env file
-const geocodingClient = mbxGeocoding({ accessToken : mapToken});//3(b) geocoding
+const geocodingClient = mbxGeocoding({ accessToken : mapToken});
 
 
 
@@ -13,7 +13,7 @@ module.exports.index = async(req,res)=>{
     res.render("./listings/index.ejs", {alllistings});
 };
 
-//3(a) 2nd lec
+//3(a) 
 module.exports.renderNewForm = (req,res)=>{
     res.render("listings/new.ejs");
 }
@@ -25,14 +25,14 @@ module.exports.showListing = async(req,res)=>{
  const listing2 =   await Listing.findById(id)
  .populate({
   path : "reviews",    
-  populate:{               //this line is added in 2(e) last lec
+  populate:{               //
       path: "author",
   },
  })
- .populate("owner");  //.populate("User") ko hmne 2(e) ke lec listing owner m add kiya
+ .populate("owner");  
  
  if(!listing2){
-  req.flash("error", "Listing you requested for does not exist!"); //3.2(c) ka flash waala part and isko hm define kiye h app.js m
+  req.flash("error", "Listing you requested for does not exist!"); 
   return res.redirect("/listings");
  }
 
@@ -44,7 +44,7 @@ module.exports.showListing = async(req,res)=>{
 
 module.exports.createListing = async(req,res,next)=>{
 
-   let response = await geocodingClient.forwardGeocode({ //3(b) lec of geocoding it is copied from document mapbox-sdk-js/docs/servies.md
+   let response = await geocodingClient.forwardGeocode({ 
     query: req.body.listing.location,
     limit: 1
   })
@@ -57,28 +57,23 @@ module.exports.createListing = async(req,res,next)=>{
 
 
 
-   let url = req.file.path;                  //3(a) save link in mongo 11th step
-   let filename = req.file.filename;          //3(a) save link in mongo 11th step
+   let url = req.file.path;                  
+   let filename = req.file.filename;          
   console.log(url, "..", filename);
 
-    // const newlist = req.body.listing; or //let {title, image, description,location, country ,price} = req.body;
-    // console.log(newlist);
-    //we are removing this if(!req.body.listing) because we are now going to use listingSchema from Schema.js file which validate server side schema
-    //   if(!req.body.listing){
-    //     throw new ExpressError(400, "send valid data for listing");
-    //   }
+  
 
      
-     const newlist = new Listing(req.body.listing);     //this will again call app.use middleware
+     const newlist = new Listing(req.body.listing);     
     
-     newlist.owner = req.user._id;  //this is 2(e) ka lec of listing owners and yahaan pr hm newlist bnane pr naye user ko add kr rhe h as a owner in our data base jis naam se vo loggedin h.
-     newlist.image = {url, filename}; // this 12th part of 3(a) lec save link in mongo
+     newlist.owner = req.user._id; 
+     newlist.image = {url, filename}; 
 
-      newlist.geometry = response.body.features[0].geometry; //3(b) m geocoding m add kiya models ke andar geometry schema bnane ke baad now go to map.js and add map marker
+      newlist.geometry = response.body.features[0].geometry; 
 
      let savedListing = await newlist.save();
      console.log(savedListing);
-    req.flash("success", "New Listing Created!"); //3. ->2(c) ka flash waala part and isko hm define kiye h app.js m
+    req.flash("success", "New Listing Created!"); 
     res.redirect("/listings"); 
   };
 
@@ -89,7 +84,7 @@ module.exports.createListing = async(req,res,next)=>{
     const listing2 =   await Listing.findById(id);
     
     if(!listing2){ //this is 2(c) ka part of flash message
-      req.flash("error", "Listing you requested for does not exist!"); //3->2(c) ka flash waala part and isko hm define kiye h app.js m
+      req.flash("error", "Listing you requested for does not exist!"); 
     return res.redirect("/listings");
   }
         //3(b) 2nd lec
@@ -106,29 +101,23 @@ module.exports.createListing = async(req,res,next)=>{
 module.exports.updateListing = async(req,res)=>{
     let {id} = req.params; 
 
-    //these lines we are adding in 2(e) lec authorization listings  //hme iss code ko har route m daalna hoga to usase better h ki iska ek middleware create krde in middleware.js m and usko har jagah add kre
-  //  let listing = await Listing.findById(id);
-  //  if(!listing.owner.equals(res.locals.currUser._id)){
-  //   req.flash("error", "you don't have permission to edit");
-  //   return res.redirect(`/listings/${id}`);
-  //  }
-  //  //end
+    
 
 
     let listing = await Listing.findByIdAndUpdate(id, {...req.body.listing});
 
-    if(typeof req.file !== "undefined"){   //3(b) 1st lec
-    let url = req.file.path;                  //3(b) 1st lec
-   let filename = req.file.filename;          //3(b) 1st lec
+    if(typeof req.file !== "undefined"){   
+    let url = req.file.path;                  
+   let filename = req.file.filename;          
 
    listing.image = {url,filename};
    await listing.save();
-    }                      // this whole code added in 3(b) 1st lec
+    }                     
 
 
 
     // res.redirect(`/listings/${id}`);
-    req.flash("success", "Listing Updated!"); //3.2(c) ka flash waala part and isko hm define kiye h app.js m
+    req.flash("success", "Listing Updated!"); 
     res.redirect(`/listings/${id}`);
 }
 
@@ -138,6 +127,6 @@ module.exports.destroyListing = async(req,res)=>{
     let {id} = req.params;
   let deletedlisting =  await Listing.findByIdAndDelete(id);
   console.log(deletedlisting);
-  req.flash("success", "Listing Deleted!"); //3.2(c) ka flash waala part and isko hm define kiye h app.js m
+  req.flash("success", "Listing Deleted!"); 
   res.redirect("/listings");
 };
